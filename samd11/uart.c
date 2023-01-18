d4dc105286d300465bb1d4b74fb3693704532b82
username <email@example.com>
2023-01-18 13:19:38 +0100

    fifo generalization

--- samd11/uart.c
+++ samd11/uart.c
@@ -23,6 +23,15 @@ typedef struct
   uint16_t  data[UART_BUF_SIZE];
 } fifo_buffer_t;
 
+#define NEXT(idx)	((idx+1)%UART_BUF_SIZE)
+#define PREV(idx)	((idx-1)%UART_BUF_SIZE)
+
+#define EMPTY(F)	(F.rd == F.wr)
+#define FULL(F)		(F.rd == NEXT(F.wr))
+
+#define PUSH(F,v)	{	F.data[F.wr] = v; F.wr = NEXT(F.wr);	}
+#define POP(F)		(	F.rd = NEXT(F.rd), F.data[PREV(F.rd)]	)
+
 /*- Variables --------------------------------------------------------------*/
 static volatile fifo_buffer_t uart_rx_fifo;
 static volatile fifo_buffer_t uart_tx_fifo;
@@ -58,14 +67,14 @@ void uart_init(usb_cdc_line_coding_t *line_coding)
 
   uart_fifo_overflow = false;
 
-  if (USB_CDC_5_DATA_BITS == line_coding->bDataBits)
-    chsize = 5;
-  else if (USB_CDC_6_DATA_BITS == line_coding->bDataBits)
-    chsize = 6;
+  if (USB_CDC_8_DATA_BITS == line_coding->bDataBits)	//most propable is first
+    chsize = 0;
   else if (USB_CDC_7_DATA_BITS == line_coding->bDataBits)
     chsize = 7;
-  else if (USB_CDC_8_DATA_BITS == line_coding->bDataBits)
-    chsize = 0;
+  else if (USB_CDC_6_DATA_BITS == line_coding->bDataBits)
+    chsize = 6;
+  else if (USB_CDC_5_DATA_BITS == line_coding->bDataBits)
+    chsize = 5;
   else
     chsize = 0;
 
@@ -116,15 +125,13 @@ void uart_close(void)
 //-----------------------------------------------------------------------------
 bool uart_write_byte(int byte)
 {
-  int wr = (uart_tx_fifo.wr + 1) % UART_BUF_SIZE;
   bool res = false;
 
   NVIC_DisableIRQ(UART_SERCOM_IRQ_INDEX);
 
-  if (wr != uart_tx_fifo.rd)
+  if (!FULL(uart_tx_fifo))
   {
-    uart_tx_fifo.data[uart_tx_fifo.wr] = byte;
-    uart_tx_fifo.wr = wr;
+    PUSH(uart_tx_fifo, byte);
     res = true;
 
     UART_SERCOM->USART.INTENSET.reg = SERCOM_USART_INTENSET_DRE;
@@ -148,10 +155,9 @@ bool uart_read_byte(int *byte)
     uart_fifo_overflow = false;
     res = true;
   }
-  else if (uart_rx_fifo.rd != uart_rx_fifo.wr)
+  else if (!EMPTY(uart_rx_fifo))
   {
-    *byte = uart_rx_fifo.data[uart_rx_fifo.rd];
-    uart_rx_fifo.rd = (uart_rx_fifo.rd + 1) % UART_BUF_SIZE;
+    *byte = POP(uart_rx_fifo);
     res = true;
   }
 
@@ -178,7 +184,6 @@ void UART_SERCOM_IRQ_HANDLER(void)
   {
     int status = UART_SERCOM->USART.STATUS.reg;
     int byte = UART_SERCOM->USART.DATA.reg;
-    int wr = (uart_rx_fifo.wr + 1) % UART_BUF_SIZE;
     int state = 0;
 
     UART_SERCOM->USART.STATUS.reg = status;
@@ -194,27 +199,25 @@ void UART_SERCOM_IRQ_HANDLER(void)
 
     byte |= (state << 8);
 
-    if (wr == uart_rx_fifo.rd)
+    if (FULL(uart_rx_fifo))
     {
       uart_fifo_overflow = true;
     }
     else
     {
-      uart_rx_fifo.data[uart_rx_fifo.wr] = byte;
-      uart_rx_fifo.wr = wr;
+      PUSH(uart_rx_fifo, byte);
     }
   }
 
   if (flags & SERCOM_USART_INTFLAG_DRE)
   {
-    if (uart_tx_fifo.rd == uart_tx_fifo.wr)
+    if (EMPTY(uart_tx_fifo))
     {
       UART_SERCOM->USART.INTENCLR.reg = SERCOM_USART_INTENCLR_DRE;
     }
     else
     {
-      UART_SERCOM->USART.DATA.reg = uart_tx_fifo.data[uart_tx_fifo.rd];
-      uart_tx_fifo.rd = (uart_tx_fifo.rd + 1) % UART_BUF_SIZE;
+      UART_SERCOM->USART.DATA.reg = POP(uart_tx_fifo);
     }
   }
 }
